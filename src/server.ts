d0a06aaa830e9dcf01ee262b35fba6bc1a867868
Renan2411 <renanmd2411@gmail.com>
2023-08-23 20:49:13 -0400

    [CONFIGURATION] - Configuração inicial do projetoo

---
+++ src/server.ts
@@ -0,0 +1,73 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json())
+const PORT = 3000;
+
+let data: any = []
+
+app.get('/', (req, res) => {
+    res.send('Olá, Mundo!');
+});
+
+app.post('/cadastrar', (req, res) => {
+    data.push(req.body)
+
+    res.status(200).json({
+        message: "Cadastrado com sucesso!"
+    })
+})
+
+app.get("/listar", (req, res) => {
+    res.status(200).json({
+        data: data
+    })
+})
+
+app.get('/listar/:id', (req, res) => {
+    const { id } = req.params
+
+    const user: any = acharUsuario(parseInt(id))
+
+    if (user) {
+        return res.status(200).json({
+            user
+        })
+    }
+
+    return res.status(400).json({
+        message: "Usuário não encontrado"
+    })
+
+})
+
+app.put('/editar/:id', (req, res) => {
+    const { id } = req.params
+    const { nome, email, telefone } = req.body
+
+    const idUsuario = Number(id) 
+
+    data = data.map((user: any) => {
+        if (user.id === idUsuario) {
+            console.log("encontrou")
+            return { id: idUsuario, nome, email, telefone }
+        }
+
+        return user
+    })
+
+    console.log(data) 
+
+
+    res.status(200).json({
+        message: 'Editado com sucesso!'
+    })
+})
+
+function acharUsuario(id: Number) {
+    return data.find((user: any) => user.id === id)
+}
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
